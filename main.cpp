99f514a0d26d01f764335c8b3978ac7ee1ab0e0a
Sahithi Kalvakota <sahithi@dhcp-rhodes-475.redrover.cornell.edu>
2019-03-10 15:38:00 -0400

    added multiset and multimap tests

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include <set>
 #include <vector>
 #include <map>
+#include <unordered_set>
 
 #include "config.h"
 #include "SequentialAllocator.h"
@@ -25,7 +26,11 @@ typedef vector<int, SequentialAllocator<int>>                     VectorType;
 typedef forward_list<int, SequentialAllocator<int>>               ForwardListType;
 typedef list<int, SequentialAllocator<int>>                       ListType;
 typedef set<int, std::less<>, SequentialAllocator<int>>           SetType;
+typedef multiset<int, std::less<>, SequentialAllocator<int>>      MultisetType;
 typedef map<int, int, std::less<>, SequentialAllocator<PairType>> MapType;
+typedef multimap<int, int, std::less<>, SequentialAllocator<PairType>> MultimapType;
+
+typedef unordered_set<int, std::hash<int>, std::equal_to<int>, SequentialAllocator<int>>    UnorderedSetType;
 
 template<class ContainerType>
 void test_container_with_insert(std::string container_name) {
@@ -112,7 +117,20 @@ void testSet() {
 }
 
 void testMultiset() {
-
+    get_offset() = 0;
+    SequentialAllocator<MultisetType> allocator;
+    SequentialAllocator<int> i_alloc{allocator};
+    MultisetType &container = *allocator.allocate(1);
+    less<> compare;
+    new (&container) MultisetType(compare, i_alloc);
+    region = container.get_allocator().get_region();
+    for (int i = 0; i < 5; i++){
+        container.insert(i);
+        container.insert(1);
+    }
+    printf("Testing multiset\n");
+    for_each(begin(container), end(container), [](int i){cout << i << " ";});
+    printf("\n\n");
 }
 
 void testMap() {
@@ -131,19 +149,44 @@ void testMap() {
 }
 
 void testMultimap() {
-
+    get_offset() = 0;
+    SequentialAllocator<MultimapType> allocator;
+    SequentialAllocator<PairType> i_alloc{allocator};
+    MultimapType &container = *allocator.allocate(1);
+    less<> compare;
+    new (&container) MultimapType(compare, i_alloc);
+    region = container.get_allocator().get_region();
+    for (int i = 0; i < 5; i++){
+        container.insert({i, i});
+        container.insert({1, i});
+    }
+    printf("Testing multimap\n");
+    for_each(begin(container), end(container), [](PairType e){cout << e.second << " ";});
+    printf("\n\n");
+    
 }
 
 void testUnorderedSet() {
-
+    get_offset() = 0;
+    SequentialAllocator<UnorderedSetType> allocator;
+    SequentialAllocator<int> i_alloc{allocator};
+    UnorderedSetType &container = *allocator.allocate(1);
+    hash<int> hashVal;
+    equal_to<int> eq_fn;
+    new (&container) UnorderedSetType(hashVal, eq_fn, i_alloc);
+    region = container.get_allocator().get_region();
+    for (int i = 0; i < 10; i++)
+        container.insert(end(container), i);
+    printf("Testing unordered set\n");
+    for_each(begin(container), end(container), [](int i){cout << i << " ";});
+    printf("\n\n");
 }
 
-void testUnorderedMultiset() {
 
+void testUnorderedMultiset() {
 }
 
 void testUnorderedMap() {
-
 }
 
 void testUnorderedMultimap() {
